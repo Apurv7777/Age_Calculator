e284b780d4f962a22ee9b0286394bf7db4b8e1b1
Apurv7777 <65327303+Apurv7777@users.noreply.github.com>
2024-01-09 13:35:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,21 @@ const btn = document.querySelector('#submit')
 const age = document.querySelector('#age')
 const msg = document.querySelector('.msg')
 
-const handleSubmit = (e)=>{
+const handleSubmit = (e) => {
     e.preventDefault();
     let inputDate = new Date(age.value).getTime();
     let currentDate = new Date().getTime();
-    let diff = currentDate-inputDate;
-    let years = Math.floor((diff / (1000 * 60 * 60 * 24))/365);
-    msg.innerHTML = 'Your age is : '+ years + '.'; 
+    if (inputDate < currentDate) {
+        let diff = currentDate - inputDate;
+        let years = Math.floor(diff / (1000 * 60 * 60 * 24));
+        if (years <= 365) {
+            msg.innerHTML = 'Your age is : ' + years + ' days.';
+        } else {
+            msg.innerHTML = 'Your age is : ' + Math.floor(years / 365) + ' years.';
+        }
+    } else {
+        msg.innerHTML = 'Please enter valid date !!';
+    }
 }
 
-btn.addEventListener('click',handleSubmit)
+btn.addEventListener('click', handleSubmit)
